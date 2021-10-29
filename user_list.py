a7d4f5b2c423c9af4f56c2c58dd884b6086014e5
ZhandosBaltabaev <90600100+ZhandosBaltabaev@users.noreply.github.com>
2021-10-29 19:28:25 +0600

    Add files via upload

---
+++ user_list.py
@@ -0,0 +1,17 @@
+nums = [5, 2, 7, "50", False]
+
+for el in nums:
+    el *= 2
+    print(el)
+
+n = int(input("Enter length: "))
+
+user_list = []
+
+i = 0
+while i < n:
+    string = "Enter element #" + str(i + 1) + ": "
+    user_list.append(input(string))
+    i += 1
+
+print(user_list)
